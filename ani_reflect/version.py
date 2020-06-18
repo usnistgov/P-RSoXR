6e548d710d427a240a15c045eb321ff817b7130e
Thomas Ferron <65238104+TomFerron@users.noreply.github.com>
2020-06-18 09:02:35 -0400

    Update version.py

--- ani_reflect/version.py
+++ ani_reflect/version.py
@@ -3,6 +3,10 @@ version = '0.1.02'
 """
 Basic functionality of ani_reflect has been implemented
 
+0.1.02 - Updated ani_structure with 'ani_NexafsSLD'
+    Allows for an SLD object to be generated form an external NEXAFS file
+    Other quality of life changes, loading data in terms of delta and beta and not a dielectric constant among others.
+
 0.1.01 - Updated ani_structure with 'ani_MaterialSLD'
     Allows for an SLD object to pull delta and beta from 'periodictable' for isotropic materials (such as substrates)
 """
